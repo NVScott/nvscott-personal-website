abeb8e24a96674fef4b30a0521d6955474dbb609
Nessa Scott <nvscott@uci.edu>
2018-12-14 13:52:40 -0800

    bar is better.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ $(window).scroll(function() {
             '-moz-box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
             'box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
             'background-color': '#bcd0f0'});
-        $(".nav-text").addClass('hidden')
+        $(".nav-text").addClass('hidden');
+        $(".menu-options").addClass('hidden');
+
 
     } else {
         $('#sticky-bar').css({
@@ -14,6 +16,8 @@ $(window).scroll(function() {
             'box-shadow': '',
             'background-color': ''});
         $(".nav-text").removeClass('hidden');
+        $(".menu-options").removeClass('hidden');
+
 
     }
 });
