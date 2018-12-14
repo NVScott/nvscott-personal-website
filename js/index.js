a29e278b53a2d1f8fd301525760aa9d55a965dc8
Nessa Scott <nvscott@uci.edu>
2018-12-13 17:58:50 -0800

    initial commit
    currently just homepage and animated sticky bar. Next to do: menu

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 129) {
+        $('#sticky-bar').css({
+            'webkit-box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
+            '-moz-box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
+            'box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
+            'background-color': '#bcd0f0'});
+        $(".nav-text").addClass('hidden')
+
+    } else {
+        $('#sticky-bar').css({
+            'webkit-box-shadow': '',
+            '-moz-box-shadow': '',
+            'box-shadow': '',
+            'background-color': ''});
+        $(".nav-text").removeClass('hidden');
+
+    }
+});
