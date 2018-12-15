c83556f8de470ee243b2476aa4cab21d60f17504
Nessa Scott <nvscott@uci.edu>
2018-12-14 17:34:35 -0800

    Experimenting with flex.
    This version has two separate sections, and I hope to flex the right size to nothing if necessary.

--- js/index.js
+++ js/index.js
@@ -5,19 +5,13 @@ $(window).scroll(function() {
             '-moz-box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
             'box-shadow': '0px 3px 16px 1px rgba(0,0,0,0.75)',
             'background-color': '#bcd0f0'});
-        $(".nav-text").addClass('hidden');
-        $(".menu-options").offcanvas();
-
-
+        $(".nav-name").addClass('hidden');
     } else {
         $('#sticky-bar').css({
             'webkit-box-shadow': '',
             '-moz-box-shadow': '',
             'box-shadow': '',
             'background-color': ''});
-        $(".nav-text").removeClass('hidden');
-        $(".menu-options").removeClass('hidden');
-
-
+        $(".nav-name").removeClass('hidden');
     }
 });
