7b60fe555eb658ee8062e2011e9c130bee087a01
Nessa Scott <nvscott@uci.edu>
2018-12-16 19:14:16 -0800

    Basic Carousel functioning
    the nav isn't working yet

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-little').slick({
+        arrows: true,
+        dots: true,
+        variableWidth: true,
+        centerMode: true,
+        infinite: true,
+        centerPadding: '60px',
+        slidesToShow: 3
+    });
+});
